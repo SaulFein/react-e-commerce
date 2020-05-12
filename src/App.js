9d57f3299d637482518f121aed5cf3d4e6f71788
Brandon <brandon.feinstein@hotmail.com>
2020-05-12 11:51:34 -0700

    header component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
 
-import "./App.css";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
-import HomePage from "./pages/homepage/homepage.component";
+import './App.css';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE </h1>
-  </div>
-);
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
+    <Header />
       <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/shop/hats" component={HatsPage} />
-
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
