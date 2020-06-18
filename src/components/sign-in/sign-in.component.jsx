d39610d940614338aec08fba6af6ff7fb831691d
Brandon <brandon.feinstein@hotmail.com>
2020-06-17 21:36:14 -0700

    added redux and implemented userReducer and user actions to header and app components

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
 import "./sign-in.styles.scss";
 
@@ -20,9 +20,17 @@ class SignIn extends React.Component {
     const { value, name } = event.target;
     this.setState({ [name]: value });
   };
-  handleSubmit = (event) => {
+
+  handleSubmit = async (event) => {
     event.preventDefault();
-    this.setState({ email: "", passsword: "" });
+
+    const {email, password} = this.state;
+    try{
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: "", passsword: "" });
+    } catch(error) {
+      console.log(error);
+    }
   };
 
   render() {
@@ -33,6 +41,7 @@ class SignIn extends React.Component {
         <form onSubmit={this.handleSubmit}>
           <FormInput
             name="email"
+            type="email"
             value={this.state.email}
             handleChange={this.handleChange}
             label="email"
@@ -40,6 +49,7 @@ class SignIn extends React.Component {
           />
           <FormInput
             name="password"
+            type="password"
             value={this.state.password}
             handleChange={this.handleChange}
             label="password"
@@ -47,7 +57,7 @@ class SignIn extends React.Component {
           />
           <div className="buttons">
             <CustomButton type="submit">Sign in</CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+            <CustomButton type="button" onClick={signInWithGoogle} isGoogleSignIn>
               {""}
               Sign in with Google{""}
             </CustomButton>
